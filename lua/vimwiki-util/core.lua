900d54689d5ecdeeeba438a125381884619632d3
umisan <t.umisan6@gmail.com>
2026-02-07 06:08:32 +0900

    add debug print

--- lua/vimwiki-util/core.lua
+++ lua/vimwiki-util/core.lua
@@ -48,6 +48,7 @@ local function wikiArchive(homedir, vimwiki_path, archive_path, line)
   local file_path = vimwiki_path .. "/" .. link_name .. ".wiki"
   print(file_path)
   local expanded_file_path, err = expandPath(file_path, homedir)
+  print(expanded_file_path)
   if err ~= nil then
     return nil, err
   end
